da1e3f25e3b686ae3627b1f72ba0cf41ee79034c
Domingo <dmm.martin@gmail.com>
2016-12-30 17:36:41 +0100

    add comment

--- sessionPrinter.blade.php
+++ sessionPrinter.blade.php
@@ -1,4 +1,12 @@
-<style>
+{{--
+    Author: Domingo Martin Mancera
+    version: 1.0.0
+    License: MIT
+    Description: Simple blade temple for displaying session variables with Laravel.
+    Year:: 2016
+--}}
+
+<style> /* Styling for the DOM elements. Based on Chrome console colors */
     pre {
         padding: 5px;
         margin: 5px;
@@ -26,23 +34,22 @@
 <script>
     var mode = "DOM"; // Option: "CONSOLE" for outputting in the console; "HIDDEN" for accessing it via var session
 
-    var data = <?php echo json_encode(session()->all());?>;
+    var data = <?php echo json_encode(session()->all());?>; //Retrieving all session variables to a JS object
 
-    if (mode == "DOM") {
-        document.body.appendChild(document.createElement('pre')).innerHTML = syntaxHighlight(JSON.stringify(data, undefined, 2));
-    }
-    if (mode == "CONSOLE") {
-        console.log(JSON.stringify(data, undefined, 2));
+    if (mode == "DOM") { // Displays variables in the DOM
+        domOutput = syntaxHighlight(JSON.stringify(data, undefined, 2));
+        document.body.appendChild(document.createElement('pre')).innerHTML = domOutput;
+    } else if (mode == "CONSOLE") {
+        console.log(JSON.stringify(data, undefined, 2)); // Displays variables in the console
         console.log('sessionPrinter is ON! Remember to switch it off in production mode!');
-    }
-    if (mode == "HIDDEN") {
-        var session = JSON.stringify(data, undefined, 2);
+    } else if (mode == "HIDDEN") {
+        var session = JSON.stringify(data, undefined, 2); // Displays variables in the console when the user types "session"
         console.log('sessionPrinter is ON! Remember to switch it off in production mode!');
     }
 
     function syntaxHighlight(json) {
         json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function(match) {
             var cls = 'number';
             if (/^"/.test(match)) {
                 if (/:$/.test(match)) {
